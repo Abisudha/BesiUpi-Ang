b70e6f7ce3036ff1efd0670d83c34805c1d19a36
Abisudha <abisudha1997@gmail.com>
2023-12-12 06:56:46 +0530

    12122023

--- src/app/common/apiUrls.ts
+++ src/app/common/apiUrls.ts
@@ -5,8 +5,8 @@ export const ApiUrls : any ={
     userApi  : 'https://retoolapi.dev/IBQleY/besiUpiUser',
     accountApi : 'https://retoolapi.dev/F4WNOn/besiUpiAccount',
     statementApi : 'https://retoolapi.dev/5n1QTH/besiUpiStatement',
-    transactionReqApi : 'https://retoolapi.dev/GFNSCL/besiUpiTransactionReq'
-
+    transactionReqApi : 'https://retoolapi.dev/GFNSCL/besiUpiTransactionReq',
+    profileApi : 'https://retoolapi.dev/Wbsefa/besiUpiProfile'
 
 }
 
--- src/app/transaction/card/card.component.ts
+++ src/app/transaction/card/card.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiUrls } from 'src/app/common/apiUrls';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
-export class CardComponent {
+export class CardComponent implements OnInit{
+
+  mobile : string ='';
+  imgName : string = '';
+  imgStatus : boolean = false;
+  profileRecord : any ;
+  imgUrl : string = '../../../assets/images/';
+
+  ngOnInit(): void {
+    this.getProfile();
+  }
+
+  constructor(private apiService : ApiService){}
+
+  getProfile(){
+    let mobile = sessionStorage.getItem('userMobile') || '';
+    let recordFilters = ['mobile='+mobile];
+    this.apiService.getApiDataByfilter(ApiUrls.profileApi, recordFilters).subscribe(
+    (data: any) => {
+      if(data != null && data.length > 0){
+        this.profileRecord = data[0];
+        this.profileRecord.imgName = this.imgUrl + this.profileRecord.imgName + ".PNG";
+      }
+    },
+    err => {
+
+    }
+    );
+  }
 
 }
